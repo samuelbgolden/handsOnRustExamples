35b23d02bd3277d055b00b3c30fde8d9eaccf306
Samuel Golden <samgolden00@gmail.com>
2022-06-28 02:07:29 -0400

    ch8 done

---
+++ dungeoncrawl/src/systems/combat.rs
@@ -0,0 +1,31 @@
+use crate::prelude::*;
+
+#[system]
+#[read_component(WantsToAttack)]
+#[write_component(Health)]
+pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
+    // get all entities who want to attack
+    let mut attackers = <(Entity, &WantsToAttack)>::query();
+
+    // get list of victim
+    let victims: Vec<(Entity, Entity)> = attackers
+        .iter(ecs)
+        .map(|(entity, attack)| (*entity, attack.victim))
+        .collect();
+
+    victims.iter().for_each(|(message, victim)| {
+        if let Ok(mut health) = ecs
+            .entry_mut(*victim)
+            .unwrap()
+            .get_component_mut::<Health>()
+        {
+            println!("Health before attack: {}", health.current);
+            health.current -= 1;
+            if health.current < 1 {
+                commands.remove(*victim);
+            }
+            println!("Health after attack: {}", health.current);
+        }
+        commands.remove(*message);
+    });
+}
